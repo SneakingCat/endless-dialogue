d8d7a8632d5e245185bdda31207c2fd83f39fa7c
Patrik Sandahl <sneakingcatsw@gmail.com>
2013-03-25 15:49:57 +0100

    Added support for reading multiple lines from the dialogue.

--- src/System/Process/EndlessDialogue.hs
+++ src/System/Process/EndlessDialogue.hs
@@ -3,16 +3,18 @@ module System.Process.EndlessDialogue (
   , open
   , tell
   , listen
+  , listenAll
   ) where
 
 import System.Process (ProcessHandle, waitForProcess, runInteractiveProcess)
 import System.Exit (ExitCode(..))
 import System.IO (Handle, hSetBinaryMode, hWaitForInput, hFlush)
-import Control.Concurrent (MVar, forkIO, newEmptyMVar, putMVar, takeMVar)
+import Control.Concurrent (Chan, forkIO, newChan, writeChan, 
+                           readChan, isEmptyChan)
 import qualified Data.ByteString.Char8 as BS
 
-type BSMVar = MVar BS.ByteString
-data Dialogue = Dialogue (Handle,Handle,Handle,ProcessHandle) BSMVar BSMVar
+type BSChan = Chan BS.ByteString
+data Dialogue = Dialogue (Handle,Handle,Handle,ProcessHandle) BSChan BSChan
 
 -- | Open a dialogue to the specified program
 open :: String -> [String] -> IO Dialogue
@@ -24,12 +26,12 @@ open cmd args = do
   hSetBinaryMode hIn False
   hSetBinaryMode hOut False
   
-  -- Create MVars to communicate with the IO managing threads
-  vIn  <- newEmptyMVar
-  vOut <- newEmptyMVar
+  -- Create Chans to communicate with the IO managing threads
+  cIn  <- newChan
+  cOut <- newChan
   
   -- Create the dialogue instance
-  let dialogue = Dialogue h vIn vOut
+  let dialogue = Dialogue h cIn cOut
   
   -- Supervise the external process
   _ <- forkIO $ supervise dialogue
@@ -43,11 +45,30 @@ open cmd args = do
 
 -- | Tell the dialogue a line of text
 tell :: Dialogue -> BS.ByteString -> IO ()
-tell (Dialogue _ vIn _) = putMVar vIn
+tell (Dialogue _ cIn _) = writeChan cIn
 
 -- | Listen to one line of text. The listen is blocking
 listen :: Dialogue -> IO BS.ByteString
-listen (Dialogue _ _ vOut) = takeMVar vOut
+listen (Dialogue _ _ cOut) = readChan cOut
+
+-- | Listen to all content that is available in the stdin buffer. The
+-- first line will be read blocking, and the rest of the content will
+-- be read non-blocking if available
+listenAll :: Dialogue -> IO [BS.ByteString]
+listenAll (Dialogue _ _ cOut) = do
+  s  <- readChan cOut
+  s' <- readChanIfAvailable cOut
+  return (s:s')
+  
+readChanIfAvailable :: BSChan -> IO [BS.ByteString]
+readChanIfAvailable cOut = do
+  emptyChan <- isEmptyChan cOut
+  case emptyChan of
+    True  -> return []
+    False -> do
+      s  <- readChan cOut
+      s' <- readChanIfAvailable cOut
+      return (s:s')
 
 -- | Supervise a started external program. Signal to the user with an
 -- io error
@@ -66,8 +87,8 @@ supervise (Dialogue (_,_,_,pid) _ _) = do
 -- | Wait for a line of text from the user to become available. Copy
 -- the content to the external process' stdin
 tellT :: Dialogue -> IO ()
-tellT d@(Dialogue (hIn,_,_,_) vIn _) = do
-  s <- takeMVar vIn
+tellT d@(Dialogue (hIn,_,_,_) cIn _) = do
+  s <- readChan cIn
   BS.hPutStrLn hIn s
   hFlush hIn
   tellT d
@@ -80,7 +101,7 @@ listenT d@(Dialogue (_,hOut,_,_) _ _) = do
   listenT' d
   
 listenT' :: Dialogue -> IO ()
-listenT' d@(Dialogue (_,hOut,_,_) _ vOut) = do
+listenT' d@(Dialogue (_,hOut,_,_) _ cOut) = do
   s <- BS.hGetLine hOut
-  putMVar vOut s
+  writeChan cOut s
   listenT' d
--- tests/TestRunner.hs
+++ tests/TestRunner.hs
@@ -6,6 +6,7 @@ import Test.HUnit
 import Test.Framework as TF (defaultMain, testGroup, Test)
 import Test.Framework.Providers.HUnit
 import System.Timeout
+import Control.Concurrent (threadDelay)
 
 main :: IO ()
 main = defaultMain tests
@@ -13,15 +14,18 @@ main = defaultMain tests
 tests :: [TF.Test]
 tests = [
   testGroup "Dialogue smoke tests" [
-     testCase "Listen shall block if no data1" blockIfNoData1
-     , testCase "Listen shall block if no data2" blockIfNoData2
+     testCase "Listen shall block if no data" blockIfNoData1
+     , testCase "Listen shall block if no data" blockIfNoData2
+     , testCase "Listen all shall block if no data" blockIfNoData3
      , testCase "Listen shall return what echo is told" asEchoIsTold
+     , testCase "Listen all shall return what echo is told" asEchoIsTold2
      , testCase "Shall follow conversation" followConversation
      , testCase "Shall follow bursty conversation" followBurstyConversation
+     , testCase "Shall follow bursty conversation " followBurstyConversation2
      ]
   ]
         
--- | There shall be not data available once the echo program has
+-- | There shall be no data available once the echo program has
 -- started. The listen command shall block
 blockIfNoData1 :: Assertion
 blockIfNoData1 = do
@@ -30,7 +34,7 @@ blockIfNoData1 = do
   assertEqual "Shall be nothing" v Nothing
   
 -- | After one tell/listen cycle to the echo program a new listen
--- command shall block du to no data
+-- command shall block due to no data
 blockIfNoData2 :: Assertion
 blockIfNoData2 = do
   d <- open echoProgram []
@@ -38,6 +42,14 @@ blockIfNoData2 = do
   tell d s
   _ <- listen d
   v <- timeout shortTimeout $ listen d
+  assertEqual "Shall be nothing" v Nothing
+  
+-- | There shall be no data available once the echo program has
+-- started. The listen all command shall block
+blockIfNoData3 :: Assertion
+blockIfNoData3 = do
+  d <- open echoProgram []
+  v <- timeout shortTimeout $ listenAll d
   assertEqual "Shall be nothing" v Nothing
   
 -- | A listen following a tell shall echo the same data as told
@@ -48,6 +60,15 @@ asEchoIsTold = do
   tell d s
   s' <- listen d
   assertEqual "Shall be equal" s s'
+  
+-- | A listen all following a tell shall echo the same data as told  
+asEchoIsTold2 :: Assertion
+asEchoIsTold2 = do
+  d <- open echoProgram []
+  let s = echoString 0
+  tell d s
+  s' <- listenAll d
+  assertEqual "Shall be equal" [s] s'
   
 -- | Follow a longer tell/listen conversation
 followConversation :: Assertion  
@@ -73,11 +94,23 @@ followBurstyConversation = do
     listenAndTest d s = do
       listen d >>= assertEqual "Shall be equal" s
   
+-- | Follow a longer bursty tell * N -> listenAll conversation
+followBurstyConversation2 :: Assertion
+followBurstyConversation2 = do
+  d <- open echoProgram []
+  let ss = map echoString [0..10]
+  mapM_ (tell d) ss
+  -- This is a quite ugly hack to let the external process get all
+  -- stuff on stout before reading
+  threadDelay shortTimeout
+  ss' <- listenAll d
+  assertEqual "Shall be equal" ss ss'
+  
 echoProgram :: String
 echoProgram = "./dist/build/Echo/Echo"
 
 shortTimeout :: Int
-shortTimeout = 1000 * 50 -- 50 ms
+shortTimeout = 1000 * 100 -- 100 ms expressed as microseconds
 
 echoString :: Int -> BS.ByteString
 echoString n = BS.pack $ "A string of text #" ++ show n
