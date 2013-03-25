0db194acd40d49d4d34edc1befa7ef69276bd7b4
Patrik Sandahl <sneakingcatsw@gmail.com>
2013-03-25 17:46:40 +0100

    Changed to use STM.TChan istead of MVar/Chan.

--- src/System/Process/EndlessDialogue.hs
+++ src/System/Process/EndlessDialogue.hs
@@ -9,12 +9,14 @@ module System.Process.EndlessDialogue (
 import System.Process (ProcessHandle, waitForProcess, runInteractiveProcess)
 import System.Exit (ExitCode(..))
 import System.IO (Handle, hSetBinaryMode, hWaitForInput, hFlush)
-import Control.Concurrent (Chan, forkIO, newChan, writeChan, 
-                           readChan, isEmptyChan)
+import Control.Concurrent (forkIO)
+import Control.Concurrent.STM (STM, atomically)
+import Control.Concurrent.STM.TChan (TChan, newTChan, writeTChan, 
+                                     readTChan, isEmptyTChan)
 import qualified Data.ByteString.Char8 as BS
 
-type BSChan = Chan BS.ByteString
-data Dialogue = Dialogue (Handle,Handle,Handle,ProcessHandle) BSChan BSChan
+type BSTChan = TChan BS.ByteString
+data Dialogue = Dialogue (Handle,Handle,Handle,ProcessHandle) BSTChan BSTChan
 
 -- | Open a dialogue to the specified program
 open :: String -> [String] -> IO Dialogue
@@ -27,8 +29,8 @@ open cmd args = do
   hSetBinaryMode hOut False
   
   -- Create Chans to communicate with the IO managing threads
-  cIn  <- newChan
-  cOut <- newChan
+  cIn  <- atomically newTChan
+  cOut <- atomically newTChan
   
   -- Create the dialogue instance
   let dialogue = Dialogue h cIn cOut
@@ -45,29 +47,30 @@ open cmd args = do
 
 -- | Tell the dialogue a line of text
 tell :: Dialogue -> BS.ByteString -> IO ()
-tell (Dialogue _ cIn _) = writeChan cIn
+tell (Dialogue _ cIn _) = atomically . (writeTChan cIn)
 
 -- | Listen to one line of text. The listen is blocking
 listen :: Dialogue -> IO BS.ByteString
-listen (Dialogue _ _ cOut) = readChan cOut
+listen (Dialogue _ _ cOut) = atomically $ readTChan cOut
 
 -- | Listen to all content that is available in the stdin buffer. The
 -- first line will be read blocking, and the rest of the content will
 -- be read non-blocking if available
 listenAll :: Dialogue -> IO [BS.ByteString]
 listenAll (Dialogue _ _ cOut) = do
-  s  <- readChan cOut
-  s' <- readChanIfAvailable cOut
-  return (s:s')
-  
-readChanIfAvailable :: BSChan -> IO [BS.ByteString]
-readChanIfAvailable cOut = do
-  emptyChan <- isEmptyChan cOut
-  case emptyChan of
+  atomically $ do
+    s  <- readTChan cOut
+    s' <- readTChanIfAvailable cOut
+    return (s:s')
+
+readTChanIfAvailable :: BSTChan -> STM [BS.ByteString]
+readTChanIfAvailable cOut = do
+  emptyChannel <- isEmptyTChan cOut
+  case emptyChannel of
     True  -> return []
     False -> do
-      s  <- readChan cOut
-      s' <- readChanIfAvailable cOut
+      s <- readTChan cOut
+      s' <- readTChanIfAvailable cOut
       return (s:s')
 
 -- | Supervise a started external program. Signal to the user with an
@@ -88,7 +91,7 @@ supervise (Dialogue (_,_,_,pid) _ _) = do
 -- the content to the external process' stdin
 tellT :: Dialogue -> IO ()
 tellT d@(Dialogue (hIn,_,_,_) cIn _) = do
-  s <- readChan cIn
+  s <- atomically $ readTChan cIn
   BS.hPutStrLn hIn s
   hFlush hIn
   tellT d
@@ -103,5 +106,5 @@ listenT d@(Dialogue (_,hOut,_,_) _ _) = do
 listenT' :: Dialogue -> IO ()
 listenT' d@(Dialogue (_,hOut,_,_) _ cOut) = do
   s <- BS.hGetLine hOut
-  writeChan cOut s
+  atomically $ writeTChan cOut s
   listenT' d
